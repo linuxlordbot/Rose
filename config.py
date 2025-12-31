1ce52a136fafad2581acaab3ddec5a0ed8b66525
Linux Lord <104729906+linuxlordbot@users.noreply.github.com>
2025-12-31 11:49:22 +0530

    config.py

--- config.py
+++ config.py
@@ -14,11 +14,11 @@ API_ID = int(os.getenv("API_ID", 0))
 API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 MONGO_URI = os.getenv("MONGO_URI", "")
-DB_NAME = os.getenv("DB_NAME", "Cluster0")
+DB_NAME = os.getenv("DB_NAME", "RoseBot")
 
 # Owner and bot details
 OWNER_ID = int(os.getenv("OWNER_ID", 0))
-BOT_USERNAME = os.getenv("BOT_USERNAME", "NomadeHelpBot")
+BOT_USERNAME = os.getenv("BOT_USERNAME", "@Gr0uppHelpBot")
 
 # Links and visuals
 SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/LearningBotsCommunity")
