20974fbf9e277b2881aba6dbf78666b38107fafd
Linux Lord <104729906+linuxlordbot@users.noreply.github.com>
2025-12-31 12:31:13 +0530

    config.py

--- config.py
+++ config.py
@@ -21,6 +21,6 @@ OWNER_ID = int(os.getenv("OWNER_ID", 0))
 BOT_USERNAME = os.getenv("BOT_USERNAME", "@Gr0uppHelpBot")
 
 # Links and visuals
-SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/LearningBotsCommunity")
-UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "https://t.me/Learning_Bots")
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/dofungroup")
+UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "https://t.me/allassetshere")
 START_IMAGE = os.getenv("START_IMAGE", "https://files.catbox.moe/j2yhce.jpg")
